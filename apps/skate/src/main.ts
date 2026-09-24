78e5ffeabc82332e1d31a97036c6003dbbc4c312
Charis Lam <26616127+charislam@users.noreply.github.com>
2026-09-24 07:53:14 -0400

    add to calendar day view

--- apps/skate/src/main.ts
+++ apps/skate/src/main.ts
@@ -6,6 +6,7 @@ import { evo } from "foldkit/struct";
 import { ActiveDate } from "./domain";
 import { Message } from "./message";
 import { Command } from "./command";
+import { cn } from "cn";
 
 // MODEL
 
@@ -44,89 +45,103 @@ export const update = (model: Model, message: Message) =>
 
 // VIEW
 
-export const view = (model: Model, h: HtmlBuilder<Message>): Document => ({
-  title: "skate",
-  body: h.div(
-    [h.Class("h-screen px-4 py-6 flex flex-col gap-8")],
-    [
-      h.hgroup(
-        [h.Class("flex items-baseline")],
-        [
-          h.h1([h.Class("text-4xl")], ["skate"]),
-          h.p([h.Class("text-sm text-slate-800 translate-y-1/4")], ["TO"]),
-        ],
-      ),
-      h.main(
-        [],
-        Match.value(model.activeDateRange).pipe(
-          Match.tagsExhaustive({
-            Initial: () => [],
-            Day: ({ date }) => [
-              h.div(
-                [h.Class("flex gap-2 justify-between")],
-                [
-                  h.h2(
-                    [h.Class("flex gap-2")],
-                    [
-                      h.span(
-                        [h.Class("text-6xl")],
-                        [Intl.DateTimeFormat("en-US", { day: "numeric" }).format(date)],
-                      ),
-                      h.span(
-                        [h.Class("py-2 flex flex-col justify-between")],
-                        [
-                          h.span(
-                            [h.Class("uppercase text-sm text-slate-600")],
-                            [Intl.DateTimeFormat("en-US", { month: "short" }).format(date)],
-                          ),
-                          h.span(
-                            [h.Class("text-slate-600 font-light tracking-wide")],
-                            [Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date)],
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  h.div(
-                    [h.Class("flex gap-4")],
-                    [
-                      h.button(
-                        [
-                          h.Class(
-                            "flex items-center text-4xl text-slate-600 hover:bg-slate-100 cursor-pointer",
-                          ),
-                          h.OnClick(Message.SelectedPreviousDateRange()),
-                        ],
-                        [
-                          h.span([h.Class("sr-only")], ["Previous"]),
-                          h.span([h.AriaHidden(true), h.InnerHTML("&#8826;")]),
-                        ],
-                      ),
-                      h.button(
-                        [
-                          h.Class(
-                            "flex items-center text-4xl text-slate-600 hover:bg-slate-100 cursor-pointer",
-                          ),
-                          h.OnClick(Message.SelectedNextDateRange()),
-                        ],
-                        [
-                          h.span([h.Class("sr-only")], ["Next"]),
-                          h.span([h.AriaHidden(true), h.InnerHTML("&#8827;")]),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-            Week: () => [],
-            Month: () => [],
-          }),
+export const view = (model: Model, h: HtmlBuilder<Message>): Document => {
+  const isDayView = model.activeDateRange._tag === "Day";
+
+  return {
+    title: "skate",
+    body: h.div(
+      [h.Class(cn("h-screen mx-auto px-4 py-6 flex flex-col gap-8", isDayView && "max-w-xl"))],
+      [
+        h.hgroup(
+          [h.Class("flex items-baseline")],
+          [
+            h.h1([h.Class("text-4xl")], ["skate"]),
+            h.p([h.Class("text-sm text-slate-800 translate-y-1/4")], ["TO"]),
+          ],
         ),
-      ),
-    ],
-  ),
-});
+        h.main(
+          [h.Class("flex-1")],
+          Match.value(model.activeDateRange).pipe(
+            Match.tagsExhaustive({
+              Initial: () => [],
+              Day: ({ date }) => [
+                h.div(
+                  [h.Class("flex gap-2 justify-between")],
+                  [
+                    h.h2(
+                      [h.Class("flex gap-2")],
+                      [
+                        h.span(
+                          [h.Class("text-6xl")],
+                          [Intl.DateTimeFormat("en-US", { day: "numeric" }).format(date)],
+                        ),
+                        h.span(
+                          [h.Class("py-2 flex flex-col justify-between")],
+                          [
+                            h.span(
+                              [h.Class("uppercase text-sm text-slate-600")],
+                              [Intl.DateTimeFormat("en-US", { month: "short" }).format(date)],
+                            ),
+                            h.span(
+                              [h.Class("text-slate-600 font-light tracking-wide")],
+                              [Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date)],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    h.div(
+                      [h.Class("flex gap-4")],
+                      [
+                        h.button(
+                          [
+                            h.Class(
+                              "flex items-center text-4xl text-slate-600 hover:bg-slate-100 cursor-pointer",
+                            ),
+                            h.OnClick(Message.SelectedPreviousDateRange()),
+                          ],
+                          [
+                            h.span([h.Class("sr-only")], ["Previous"]),
+                            h.span([h.AriaHidden(true), h.InnerHTML("&#8826;")]),
+                          ],
+                        ),
+                        h.button(
+                          [
+                            h.Class(
+                              "flex items-center text-4xl text-slate-600 hover:bg-slate-100 cursor-pointer",
+                            ),
+                            h.OnClick(Message.SelectedNextDateRange()),
+                          ],
+                          [
+                            h.span([h.Class("sr-only")], ["Next"]),
+                            h.span([h.AriaHidden(true), h.InnerHTML("&#8827;")]),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+              Week: () => [],
+              Month: () => [],
+            }),
+          ),
+        ),
+        h.footer(
+          [],
+          [
+            h.button([
+              h.Class("text-sm text-slate-600 hover:underline underline-offset-2 cursor-pointer"),
+              h.OnClick(Message.SelectedCurrentDateRange()),
+              h.InnerHTML("Go to today &rarr;"),
+            ]),
+          ],
+        ),
+      ],
+    ),
+  };
+};
 
 // INIT
 
